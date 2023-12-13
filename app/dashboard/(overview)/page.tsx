4ec1b5519994cd0fe22d2d394b02a4b1725a015e
Arthur Candido <oarthurcandido@gmail.com>
2023-12-13 17:58:06 -0300

    :sparkles: finishing

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -16,7 +16,6 @@ export default async function Page() {
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         Dashboard
-        <Search placeholder='Busque'></Search>
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
         <Suspense fallback={<CardSkeleton/>}>
